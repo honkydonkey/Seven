605cad5be90154e42ac37dbe24c0560a0861b4a4
Arata Honda <havoc0214@yahoo.co.jp>
2015-06-30 21:24:57 +0900

    Create SevensMain.java

---
+++ SevensMain.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author f1110800661
+ */
+public class SevensMain 
+{
+    public static void main(String[] args)
+    {
+        Administar adm = new Administar();
+        adm.start();
+    }
+}
